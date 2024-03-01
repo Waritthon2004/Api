9116f7a8c59ab8a769b82ca881c372e873247f79
Waritthon2004 <Waritthonzaza@gmail.com>
2024-03-01 23:26:51 +0700

    Update image.ts

--- api/image.ts
+++ api/image.ts
@@ -4,14 +4,7 @@ import mysql from "mysql";
 //router = ตัวจัดการเส้นทาง
 export const router = express.Router();
 
-router.get("/", (req, res) => {
-    let sql = "SELECT url FROM Picture ORDER BY RAND() LIMIT 2";
-    conn.query(sql, (err, result) => {
-        if (err) throw err;
-       
-        res.status(200).json({image1 : result[0].url, image2 : result[1].url});
-    })
-  });
+
   
   router.get("/", (req, res) => {
     let sql = "SELECT * FROM Picture ORDER BY RAND() LIMIT 2";
