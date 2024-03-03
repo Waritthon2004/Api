d75aa4752768930499779047d1b5356878ba6557
Khanomtom <146657653+Khanomtom@users.noreply.github.com>
2024-03-03 20:19:31 +0700

    gg

--- api/image.ts
+++ api/image.ts
@@ -4,7 +4,7 @@ import mysql from "mysql";
 //router = ตัวจัดการเส้นทาง
 export const router = express.Router();
   
-  router.get("/", (req, res) => {
+  router.get("/all", (req, res) => {
     let sql = "SELECT * FROM Picture";
     conn.query(sql, (err, result) => {
       if (err) throw err;
@@ -13,7 +13,24 @@ export const router = express.Router();
         .json(result);
     });
   });
+  router.get("/", (req, res) => {
+    let sql = "SELECT * FROM Picture ORDER BY RAND() LIMIT 2";
+    conn.query(sql, (err, result) => {
+      if (err) throw err;
+      res
+        .status(200)
+        .json({
+          pid1: result[0].PID,
+          image1: result[0].url,
+          point1: result[0].point,
   
+          image2: result[1].url,
+          point2: result[1].point,
+          pid2: result[1].PID
+        });
+    });
+  });
+
   router.delete("/:id", (req, res) => {
     const id = req.params.id;
     console.log(id);
