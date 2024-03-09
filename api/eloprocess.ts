62388613847bf429ccd977cc7b00dda376894e12
Waritthon2004 <Waritthonzaza@gmail.com>
2024-03-09 21:46:00 +0700

    elo update

--- api/eloprocess.ts
+++ api/eloprocess.ts
@@ -29,52 +29,57 @@ router.put("/",async (req, res) => {
   if(pointB <=0){
     pointB = 0;
   }
+  const currentDate = new Date().toISOString().slice(0, 10);
   let check1: any = await new Promise((resolve, reject) => {
-    conn.query("select * from Statics where `Dete` = ?  where `PID` = ?", [], (err, result) => {
+    conn.query("select SID from Statics where `Date` = ?  and `PID` = ?", [currentDate,image.PID1], (err, result) => {
       if (err) reject(err);
       resolve(result);
     });
   });
 
   let check2: any = await new Promise((resolve, reject) => {
-    conn.query("select SID from Statics where `Dete` = ?  where `PID` = ?", [], (err, result) => {
+    conn.query("select SID from Statics where `Date` = ?  and `PID` = ?", [currentDate,image.PID2], (err, result) => {
       if (err) reject(err);
       resolve(result);
     });
   });
-  const currentDate = new Date().toISOString().slice(0, 10);
+
 
   let sql1 = "";
   let sql2 = "";
-
-  if(check1){
+ 
+  if(check1.length>0){
     sql1 = "update Statics set `point` = ?  where `SID` = ?";
     sql1 = mysql.format(sql1, [pointA, image.PID1]);
   }
   else{
     sql1 = "INSERT INTO `Statics`(`PID`, `Date`, `point`) VALUES (?,?,?)";
-    sql1 = mysql.format(sql1, [pointA,currentDate, image.PID1]);
+    sql1 = mysql.format(sql1, [image.PID1,currentDate, pointA]);
   }
 
-  if(check2){
+  if(check2.length>0){
      sql2 = "update Statics set `point` = ?  where `SID` = ?";
     sql2 = mysql.format(sql2, [pointB,image.PID2]);
   }
   else{
     sql2 = "INSERT INTO `Statics`(`PID`, `Date`, `point`) VALUES (?,?,?)";
-    sql2 = mysql.format(sql2, [pointB, currentDate,image.PID2]);
+    sql2 = mysql.format(sql2, [image.PID2, currentDate,pointB]);
   }
+
+  console.log(sql1);
+  console.log(sql2);
+  
   Promise.all([
     new Promise((resolve, reject) => {
       conn.query(sql1, (err, result) => {
         if (err) reject(err);
-        resolve(result);
+        resolve( result.affectedRows);
       });
     }),
     new Promise((resolve, reject) => {
       conn.query(sql2, (err, result) => {
         if (err) reject(err);
-        resolve(result);
+        resolve( result.affectedRows);
       });
     })
   ])
