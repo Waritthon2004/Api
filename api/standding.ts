1610b2e54855461d9d052d577d7e988bf2190fdc
Khanomtom <146657653+Khanomtom@users.noreply.github.com>
2024-03-17 01:12:19 +0700

    new

--- api/standding.ts
+++ api/standding.ts
@@ -6,9 +6,13 @@ export const router = express.Router();
 
 
 router.get("/", (req, res) => {
-    let sql = `SELECT *, ROW_NUMBER() OVER (ORDER BY point DESC) AS "rank" FROM Statics,Picture 
-    WHERE  Picture.PID = Statics.PID
-     ORDER BY point DESC LIMIT 10`;
+    let sql = `SELECT PID, point, url, ROW_NUMBER() OVER (ORDER BY point DESC) AS "rank" 
+    FROM (
+        SELECT Picture.PID, MAX(Statics.point) AS point, MAX(url) AS url 
+        FROM Statics, Picture 
+        WHERE Picture.PID = Statics.PID AND Statics.Date = (SELECT MAX(Date) FROM Statics) GROUP BY 	  	Picture.PID 
+    ) 
+    AS max_points ORDER BY point DESC LIMIT 10`;
     conn.query(sql, (err, result) => {
       if (err) throw err;
       res
@@ -18,9 +22,13 @@ router.get("/", (req, res) => {
   });
 
   router.get("/pervious", (req, res) => {
-    let sql = `SELECT *, ROW_NUMBER() OVER (ORDER BY point DESC) AS "rank" FROM Statics,Picture 
-    WHERE  Picture.PID = Statics.PID
-    AND DATEDIFF(CURDATE(),Date)=1 ORDER BY point DESC LIMIT 10
+    let sql = `SELECT PID, point, url, ROW_NUMBER() OVER (ORDER BY point DESC) AS "rank" 
+    FROM (
+        SELECT Picture.PID, MAX(Statics.point) AS point, MAX(url) AS url 
+        FROM Statics, Picture 
+        WHERE Picture.PID = Statics.PID AND Statics.Date = (SELECT MAX(Date)-1 FROM Statics) GROUP BY 	  	Picture.PID 
+    ) 
+    AS max_points ORDER BY point DESC LIMIT 10
     `;
     conn.query(sql, (err, result) => {
       if (err) throw err;
